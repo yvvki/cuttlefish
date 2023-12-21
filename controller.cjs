da837df08e482c9c9fc57a1b13a26fdd0d02c126
Yuuki Rika <yvvki@duck.com>
2023-12-21 16:15:01 +0700

    Remove `setLayoutZoomLevelLimits`
    See https://github.com/electron/electron/pull/21296

--- controller.cjs
+++ controller.cjs
@@ -302,4 +302,3 @@ const controller = function (parameter, value) {
 const webFrame = require('electron').webFrame;
 
 webFrame.setVisualZoomLevelLimits(1, 1);
-webFrame.setLayoutZoomLevelLimits(0, 0);
