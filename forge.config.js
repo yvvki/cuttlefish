abeb39840e2ed5b625d2157d283cb71b915e1ecb
Yuuki Rika <yvvki@duck.com>
2023-12-21 11:43:21 +0700

    Format Electron Forge configuration

--- forge.config.js
+++ forge.config.js
@@ -1,34 +1,35 @@
+/* eslint-disable unicorn/prefer-module */
 /**
  * @type {import('@electron-forge/shared-types').ResolvedForgeConfig}
  */
 module.exports = {
-  packagerConfig: {
-    appBundleId: 'id.synth.cuttlefish',
-    asar: true,
-  },
-  rebuildConfig: {},
-  makers: [
-    {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
-  ],
-  plugins: [
-    {
-      name: '@electron-forge/plugin-auto-unpack-natives',
-      config: {},
-    },
-  ],
+	packagerConfig: {
+		appBundleId: 'id.synth.cuttlefish',
+		asar: true,
+	},
+	rebuildConfig: {},
+	makers: [
+		{
+			name: '@electron-forge/maker-squirrel',
+			config: {},
+		},
+		{
+			name: '@electron-forge/maker-zip',
+			platforms: ['darwin'],
+		},
+		{
+			name: '@electron-forge/maker-deb',
+			config: {},
+		},
+		{
+			name: '@electron-forge/maker-rpm',
+			config: {},
+		},
+	],
+	plugins: [
+		{
+			name: '@electron-forge/plugin-auto-unpack-natives',
+			config: {},
+		},
+	],
 };
